808df386fcbdb76a157ec9612022ce12c52b1b36
Pavlunm <Pavlun_m@icloud.com>
2026-04-18 10:08:46 +0300

    создание лессон 2 таска 1

--- src/Lesson2Task1.java
+++ src/Lesson2Task1.java
@@ -5,6 +5,6 @@ public class Lesson2Task1 {
     public static void printThreeWords() {
         System.out.println("Orange");
         System.out.println("Banana");
-        System.out.println("Apple1");
+        System.out.println("Apple");
     }
 }

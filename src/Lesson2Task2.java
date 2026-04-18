71903fd8a5bd8cb59e0f84ebd5871386ea41d9cc
Pavlunm <Pavlun_m@icloud.com>
2026-04-18 11:31:46 +0300

    создание дз 2.2 таска 1 2 3

--- src/Lesson2Task2.java
+++ src/Lesson2Task2.java
@@ -1,2 +1,15 @@
 public class Lesson2Task2 {
+    public static void main(String[] args) {
+        checkSumSign();
+    }
+    public static void checkSumSign() {
+        int a = 10;
+        int b = -5;
+        int sum = a + b;
+        if (sum >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
 }
--- src/Lesson2Task3.java
+++ src/Lesson2Task3.java
@@ -1,2 +1,15 @@
 public class Lesson2Task3 {
+    public static void main(String[] args) {
+        printColor();
+    }
+    public static void printColor() {
+        int value = 50;
+        if (value <= 0) {
+            System.out.println("Красный");
+        } else if (value <= 100) {
+            System.out.println("Желтый");
+        } else {
+            System.out.println("Зеленый");
+        }
+    }
 }

1ae87c2d6af2ce5f7b206d4d176ce82a747e2f8d
Pavlunm <Pavlun_m@icloud.com>
2026-05-17 17:17:37 +0300

    дз 11

--- src/test/java/org/example/lesson11/BaseTest.java
+++ src/test/java/org/example/lesson11/BaseTest.java
@@ -1,15 +1,8 @@
 package org.example.lesson11;
 
-import io.qameta.allure.Allure;
 import org.example.lesson11.service.MtsHomePageService;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.WebDriver;
-import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-
-import java.io.ByteArrayInputStream;
 
 public abstract class BaseTest {
 
@@ -21,21 +14,7 @@ public abstract class BaseTest {
     }
 
     @AfterMethod(alwaysRun = true)
-    public void tearDown(ITestResult result) {
-        if (result.getStatus() == ITestResult.FAILURE) {
-            attachScreenshot("Screenshot on failure");
-        }
+    public void tearDown() {
         DriverManager.quitDriver();
-    }
-
-    private void attachScreenshot(String name) {
-        try {
-            WebDriver driver = DriverManager.getDriver();
-            if (driver instanceof TakesScreenshot screenshotDriver) {
-                byte[] screenshot = screenshotDriver.getScreenshotAs(OutputType.BYTES);
-                Allure.addAttachment(name, "image/png", new ByteArrayInputStream(screenshot), "png");
-            }
-        } catch (Exception ignored) {
-        }
     }
 }
